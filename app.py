24a0669ffce94c3adfb3bf3a92f658a404c80033
dikshant6398 <dikshantsharma140@gmail.com>
2025-06-25 09:23:44 +0000

    Feature:Implement the login feature and fix get_user funtionality

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask , request
-from login_user import Fetch
-from user_login import user_login
 from flask_cors import CORS 
-from create_user import create_user
 app = Flask(__name__)
-CORS(app) 
+# CORS(app) 
 
-
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def welcome():
@@ -15,33 +11,7 @@ def welcome():
     return 'hello world'
 
 
-
-# @app.route("/fetch")
-# def fetch_user():
-#     f = Fetch()
-#     result = f.get_users()
-#     return result
-
-# from controller import *
 from controller import user_controller
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# @app.route('/update/<id>', methods=['POST'])
-# def update(id):
-#     f = Fetch()
-#     result = f.update_user(id,request.form)
-#     return result   
-
-# @app.route('/add',methods=['POST'])
-# def add():
-#     try:
-#         try:
-#             f = Fetch()
-#         except:
-#             return " object not created"
-#         # result = f.add_user(request.form)
-#         return f.add_user(request.form)   
-#     except:
-#         return "route error"
--- controller/user_controller.py
+++ controller/user_controller.py
@@ -6,27 +6,32 @@ model_obj = User_model()
 
 @app.route('/user/signup', methods=['POST'])
 def user_signup():
+    data = request.get_json()
     model_obj = User_model()
-    return model_obj.user_signup_logic(request.form)
+    return model_obj.user_signup_logic(data)
 
-@app.route('/user/login',methods=['PUT'])
+@app.route('/user/login',methods=['POST'])
 def user_login():
+    data = request.get_json()
     model_obj = User_model()
-    return model_obj.user_login_logic(request.form)
+    return model_obj.user_login_logic(data)
 
 
-@app.route("/user/getall")
+@app.route("/user/getall" ,methods=['POST'])
 def user_getall():
+    data = request.get_json()
     model_obj = User_model()
-    return model_obj.user_getall_logic()
+    return model_obj.user_getall_logic(data)
 
 @app.route("/user/update", methods=['PUT'])
 def user_update():
+    data = request.get_json()
     model_obj = User_model()
-    return model_obj.user_update_logic(request.form)
+    return model_obj.user_update_logic(data)
 
 
 @app.route("/user/delete/<int:id>", methods=['DELETE'])
 def user_delete(id):
+    data = request.get_json()
     model_obj = User_model()
     return model_obj.user_delete_logic(id)
